2a6fd40fb6eb82fb3fa0ba543b873dc6af98a464
CassioGuimaraes <131621517+CassioSobreira@users.noreply.github.com>
2025-11-08 23:22:24 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+import 'dotenv/config'; 
 import express, { Express, Request, Response, NextFunction } from 'express';
-import 'dotenv/config';
+import cors from 'cors'; 
 
 console.log('>>> [INDEX] Tentando ler PORT do .env:', process.env.PORT);
 console.log('>>> [INDEX] Tentando ler JWT_SECRET do .env:', process.env.JWT_SECRET ? 'Encontrado!' : 'NÃO ENCONTRADO!');
 
-import connectDB from './database/configdb';
+import connectDB from './database/configdb'; 
 import authRoutes from './routes/authRoutes';
 import movieRoutes from './routes/movieRoutes'; 
 
@@ -14,27 +15,24 @@ const PORT = process.env.PORT || 3001;
 
 connectDB();
 
+app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
-
 app.get('/', (req: Request, res: Response) => {
-  console.log(">>> [INDEX] Rota / ACESSADA!");
-  res.send('API de Autenticação e Filmes v1.1 está funcionando!'); 
+  console.log(">>> [INDEX] Rota / ACESSADA!");
+  res.send('API de Autenticação e Filmes v1.1 (MongoDB) está funcionando!'); 
 });
 
-
-app.use('/api/auth', authRoutes);   
+app.use('/api/auth', authRoutes);   
 app.use('/api/movies', movieRoutes); 
 
-
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(">>> [INDEX] ERRO GLOBAL NÃO TRATADO:", err.stack || err); 
-  res.status(err.status || 500).json({ 
-    message: err.message || 'Erro interno inesperado no servidor.',
-  });
+  console.error(">>> [INDEX] ERRO GLOBAL NÃO TRATADO:", err.stack || err); 
+  res.status(err.status || 500).json({ 
+    message: err.message || 'Erro interno inesperado no servidor.',
+  });
 });
 
 app.listen(PORT, () => {
-  console.log(`>>> [INDEX] Servidor INICIADO na porta ${PORT}`);
+  console.log(`>>> [INDEX] Servidor INICIADO na porta ${PORT}`);
 });
-
